a93c465597fba390403f8a0bea4f39c5b0012423
joshua chung <joshuachung@ucsb.edu>
2024-05-28 17:47:21 -0700

    test

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # FamilyTree
-This program is a way to show/idenify the information and position for the relatives that are within your family. 
+This program I have built a database program that stores a collection of
+people and  their biological relatives.  You can then search this database based
+on the relationships between the people it contains. 
 # This Program Should : 
 - identify the positions/roles of each relative
 - inform the user the decendents of a relative
 - inform the user the siblings of a relative
 - inform the users specific information of the relative, this can include `birthdays` and `favorite food` or even `pet names`.
+
+# How to use the program
+When in terminal after you run the program using some type of input like this `./test data/Family.tsv`
+
+your input should look something simular to this : 
+`name's input` - the input can be anything to identify what you want to look into, this including `siblings`, `parents`, `cousins`, `nephews`, etc.
--- family.cpp
+++ family.cpp
@@ -1,1 +1,54 @@
 
+#include "family.h"
+// family Member Functions
+#include <sstream>
+
+// Destructor
+GenePool::~GenePool() {
+    for (auto& pair : m_people) {
+        delete pair.second;
+    }
+}
+
+// Constructor
+GenePool::GenePool(std::istream& stream) {
+    std::string line;
+    while (std::getline(stream, line)) {
+        if (line.empty() || line[0] == '#') continue;
+
+        std::istringstream ss(line);
+        std::string name, gender, motherName, fatherName;
+        std::getline(ss, name, '\t');
+        std::getline(ss, gender, '\t');
+        std::getline(ss, motherName, '\t');
+        std::getline(ss, fatherName, '\t');
+
+        addPerson(name, gender == "male" ? Gender::MALE : Gender::FEMALE, motherName, fatherName);
+    }
+}
+
+// Adding a person into the family tree data base / Updating information
+void GenePool::addPerson(const std::string& name, Gender gender, const std::string& motherName, const std::string& fatherName) {
+    Person* mother = (motherName != "???") ? find(motherName) : nullptr;
+    Person* father = (fatherName != "???") ? find(fatherName) : nullptr;
+    Person* person = new Person(name, gender, mother, father);
+    m_people[name] = person;
+
+    if (mother) mother->m_children.insert(person);
+    if (father) father->m_children.insert(person);
+}
+
+// Listing everyone in the database/Tree
+std::set<Person*> GenePool::everyone() const {
+    std::set<Person*> result;
+    for (const auto& pair : m_people) {
+        result.insert(pair.second);
+    }
+    return result;
+}
+
+// Locate the person in the database/family Tree 
+Person* GenePool::find(const std::string& name) const {
+    auto it = m_people.find(name);
+    return it != m_people.end() ? it->second : nullptr;
+}
